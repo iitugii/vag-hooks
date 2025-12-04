2c9168404be7a3e4d0468a39a51a521455e36423
iitugii <winsor.lozano@gmail.com>
2025-12-04 14:43:08 -0500

    Update paysheet layout and service percentage defaults

--- src/routes/employees.ts
+++ src/routes/employees.ts
@@ -1,5 +1,7 @@
-export const providerDirectory: Record<string, string> = {
-	"0U~8qaluNm4nlBHS1lX4Sg==": "Yully Gonzalez",
+export type ProviderDirectory = Record<string, string>;
+
+export const providerDirectory: ProviderDirectory = {
+    "0U~8qaluNm4nlBHS1lX4Sg==": "Yully Gonzalez",
     "VJYp1jmJ990M8rSgmEKuCg==": "Leidy Villasmil",
     "ygttmkB0kqajnP3G9VFFCA==": "Elizabeth Rodriguez",
     "HQVVjyfx2LMBmjykVMp4uA==": "Liz Gutierrez",
@@ -18,14 +20,29 @@ export const providerDirectory: Record<string, string> = {
     "1f0-pADxzjo5oCJfQvqwEg==": "Elianys Vigoa",
     "cOuMhKYYXwez7hlBoLpnPg==": "Lily S",
     "2~XMU4-eycP4LHBPEzpyVQ==": "Orianna Taborda"
+};
 
-
-
-
-
-
+export const providerServicePercentages: Record<string, number> = {
+    "0U~8qaluNm4nlBHS1lX4Sg==": 45,
+    "2~XMU4-eycP4LHBPEzpyVQ==": 45,
+    "DYOsnZF92isbHl0aVQtB5w==": 45,
+    "hWJIChYtRdsuqlnAeIpqfA==": 50,
+    "HQVVjyfx2LMBmjykVMp4uA==": 50,
+    "RgW1Yi3geC8oYpX-jPt1xg==": 50,
+    "G~vAH-ywuaMTU8U-mQSPSw==": 45,
+    "RL~9TokljBGmTZJrF~OONQ==": 45,
+    "VJYp1jmJ990M8rSgmEKuCg==": 45,
+    "QAAiKbzzli94LGrnJ3GX0Q==": 45,
+    "ygttmkB0kqajnP3G9VFFCA==": 45,
+    "u41~oI~Elac3QmDg0IE3LQ==": 50,
+    "1f0-pADxzjo5oCJfQvqwEg==": 50,
+    "XYSdsoZNLpg~vz42OYwQPg==": 45,
+    "qloWTWPGZbaXLm62UEZezw==": 45,
+    "8m0Pi9tJgReoyUxrRqq-Nw==": 50,
+    "iGdlKrBiZNl~zAXcKYHAtg==": 45,
+    "lLHKiHNGeN0d-CmqhnUIzg==": 45
 };
 
 export function lookupProviderName(providerId: string): string | undefined {
-	return providerDirectory[providerId];
+    return providerDirectory[providerId];
 }
